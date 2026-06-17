785ccc2225fd4edcb3075ff9a2205f73a507b66c
Lucas <lucasdfso@gmail.com>
2026-06-17 09:44:28 -0300

    Add 'Testing App' SwiftUI Xcode project
    Add a new Xcode SwiftUI app 'Testing App' including project and workspace files (project.pbxproj, contents.xcworkspacedata), asset catalogs (AppIcon, AccentColor, Assets.xcassets), app sources (Testing_AppApp.swift, ContentView.swift) and test targets with stubs (unit and UI tests). Also update .gitignore to include .DS_Store. This commit bootstraps the initial project template and configuration.

---
+++ Testing App/Testing App/Testing_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Testing_AppApp.swift
+//  Testing App
+//
+//  Created by Lucas Oliveira on 17/06/26.
+//
+
+import SwiftUI
+
+@main
+struct Testing_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Testing App/Testing AppTests/Testing_AppTests.swift
@@ -0,0 +1,19 @@
+//
+//  Testing_AppTests.swift
+//  Testing AppTests
+//
+//  Created by Lucas Oliveira on 17/06/26.
+//
+
+import Testing
+@testable import Testing_App
+
+struct Testing_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
